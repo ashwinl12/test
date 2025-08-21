94634f12f9f5fa680c4fc8f882b141ccee291882
Ashwini Kumar <ashwini.kumar@joshsoftware.com>
2025-08-22 01:23:18 +0530

    breaking bad

--- greet.go
+++ greet.go
@@ -3,10 +3,14 @@ package test
 import "fmt"
 
 
-func Greet(name string) string {
-	return fmt.Sprintf("Hi there, %s !!\n%s", name, Intro())
-}
+// func Greet(name string) string {
+// 	return fmt.Sprintf("Hi there, %s !!\n%s", name, Intro())
+// }
 
-func Intro() string {
-	return "Welcome to demo !!"
+// func Intro() string {
+// 	return "Welcome to demo !!"
+// }
+
+func Bye(name string) string {
+	return fmt.Sprintf("Bye %s !!", name)
 }
